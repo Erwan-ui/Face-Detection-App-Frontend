bc5bc09508ff3f66f2201f4c3a1acb79f0ff518c
Erwan-ui <62938950+Erwan-ui@users.noreply.github.com>
2020-05-18 12:48:31 +0200

    Updates

---
+++ precache-manifest.36698ae54d66b3507a55b3e57e1f3494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fe0ecd295a2dc0421515de28f7b12c1",
+    "url": "/Face-Detection-App-Frontend/index.html"
+  },
+  {
+    "revision": "b29aa51f0bf26d236e75",
+    "url": "/Face-Detection-App-Frontend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "8149e45cb944d9670fde",
+    "url": "/Face-Detection-App-Frontend/static/css/main.256529bb.chunk.css"
+  },
+  {
+    "revision": "b29aa51f0bf26d236e75",
+    "url": "/Face-Detection-App-Frontend/static/js/2.6dbd4bb6.chunk.js"
+  },
+  {
+    "revision": "78187008a3faaca3f802566a5181872e",
+    "url": "/Face-Detection-App-Frontend/static/js/2.6dbd4bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8149e45cb944d9670fde",
+    "url": "/Face-Detection-App-Frontend/static/js/main.f8a1bca4.chunk.js"
+  },
+  {
+    "revision": "dfa927b8f48d803728ab",
+    "url": "/Face-Detection-App-Frontend/static/js/runtime-main.0c69ddc8.js"
+  }
+]);
